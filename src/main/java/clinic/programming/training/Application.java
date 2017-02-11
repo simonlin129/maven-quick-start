a12ab0c25727285fb4462f3913aedc19f1913f70
simonlin129@gmail.com <simonlin129@gmail.com>
2017-02-11 03:26:55 -0500

    Use StringUtils

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,15 +1,23 @@
 package clinic.programming.training;
 
 import java.util.*;
+import org.apache.commons.lang3.StringUtils;
+
 
 public class Application {
-    
+
+
+public int countWords(String words){
+String [] separateWords = StringUtils.split(words, ' ');
+return (separateWords == null) ? 0 : separateWords.length;	
+}	
+	
 public void greet(){
+	
 List<String> greetings =new ArrayList<>();
 greetings.add("Hello");
 
-for (String greeting: greetings)
-{
+for (String greeting: greetings){
 System.out.println("Greeting" +greeting);
 }
 
@@ -24,6 +32,8 @@ public Application()
 public static void main (String[] args) {
 System.out.println ("Starting Application");
 Application app = new Application();
-app.greet();
+//app.greet();
+int count =app.countWords("I have four words");
+System.out.println("Word Count: " + count);
 }
 }
