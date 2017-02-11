775155578bfbb305daadf24f476c797e72af9ce2
simonlin129@gmail.com <simonlin129@gmail.com>
2017-02-11 02:01:54 -0500

    Changing Plugin

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,7 +1,20 @@
 package clinic.programming.training;
 
+import java.util.*;
+
 public class Application {
     
+public void greet(){
+List<String> greetings =new ArrayList<>();
+greetings.add("Hello");
+
+for (String greeting: greetings)
+{
+System.out.println("Greeting" +greeting);
+}
+
+}	
+	
 public Application() 
 {
         System.out.println ("Inside Application");
@@ -11,5 +24,6 @@ public Application()
 public static void main (String[] args) {
 System.out.println ("Starting Application");
 Application app = new Application();
+app.greet();
 }
 }
